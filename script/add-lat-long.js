e4062a8ad7903c83fdc2650530ec3ff5fb10b894
leomcelroy <leomcelroy@gmail.com>
2022-12-12 13:48:30 -0500

    vite and cron job

---
+++ script/add-lat-long.js
@@ -0,0 +1,20 @@
+import { searchAddress, searchLatLong } from "./searchAddress.js";
+import airtableHelperFactory from "./airtable.js";
+
+const sleep = m => new Promise(r => setTimeout(r, m))
+
+const { get, update } = airtableHelperFactory("Clubs Dashboard", "Main");
+
+const all = await get();
+
+for (const club of all) {
+	if (!club.fields["Location"] || club.fields["Location"] === "" || club.fields["Latitude"]) continue;
+	const locationString = club.fields["Location"];
+	const [ Latitude, Longitude ] = await searchLatLong(locationString);
+	if (Latitude == 0 && Longitude == 0) continue;
+
+	console.log("Updating", club.fields["Venue"]);
+
+	update({ id: club.id, fields: { Latitude, Longitude } });
+	await sleep(1000);
+}
---
+++ script/searchAddress.js
@@ -0,0 +1,30 @@
+import Bottleneck from 'bottleneck';
+import fetch from "node-fetch";
+
+const limiter = new Bottleneck({
+  maxConcurrent: 1,
+  minTime: 1000
+})
+
+export const searchAddress = async (address) => {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAP_API}`
+    const json = await fetch(url).then( res => res.json() );
+    
+    return json.results.length > 0 ? json.results[0] : [];
+}
+
+export const searchLatLong = async (address) => {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAP_API}`
+    const json = await fetch(url).then( res => res.json() );
+    
+    let lat = 0;
+    let lng = 0;
+
+    if (json.results.length > 0) {
+        const loc = json.results[0].geometry.location
+        lat = loc.lat;
+        lng = loc.lng;
+    }
+    
+    return [ lat, lng ];
+}
